7f43e24ec2f1aed2ea4c4fb5e6e4cce8e939c144
Aeroses <thatalexrose@gmail.com>
2020-12-17 22:19:33 -0800

    Update Fractal.js

--- src/Fractal.js
+++ src/Fractal.js
@@ -15,9 +15,10 @@ export class Fractal {
         let imgData = this._ctx.createImageData(width, height);
 
         //scale to screen orientation
-        let stretch = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-            ? screen.width / screen.height
-            : width / height;
+        // let stretch = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+        //     ? screen.width / screen.height
+        //     : width / height;
+        let stretch = window.innerWidth / window.innerHeight;
 
         console.log(`W: ${width}, H${height}`);
         //mandelbrot formula
